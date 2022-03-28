df80b3074b4882d607587d3c8e413a1965cff4c4
chemical-saw <weerawatboonhad292531@gmail.com>
2022-03-28 17:39:21 +0700

    EP6.1 39.58

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+
+
+  // General
+  static String appName = 'Shopping Mall';
+
+  // Route
+  static String routerAuthen = '/authen';
+  static String routerCreateAccount = '/createAccount';
+  static String routerBuyerService = 'buyerService';
+  static String routerSalerService = 'salerService';
+  static String routerRiderService = 'riderService';
+  
+}
